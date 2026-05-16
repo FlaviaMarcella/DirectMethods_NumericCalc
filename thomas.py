35238c839111213b9577a2f37d51974a0593a80e
Flávia Gonçalves <flavia.marcella@alunos.ifsuldeminas.edu.br>
2026-05-16 08:08:48 -0300

    Ajuste

--- thomas.py
+++ thomas.py
@@ -1,6 +1,6 @@
 """
 Módulo: thomas.py
-Descrição: Implementa o Algoritmo de Thomas (TDMA) de complexidade linear O(n), 
+Descrição: Implementaa o Algoritmo de Thomas (TDMA) de complexidade linear O(n), 
 otimizado para a resolução de sistemas de equações tridiagonais.
 """
 
